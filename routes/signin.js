2f045887ec448b2d3612d68b17f4c281b54a4037
Mike Menendez <mmenendez@csumb.edu>
2019-12-16 02:26:30 -0800

    dfjk

--- routes/signin.js
+++ routes/signin.js
@@ -56,9 +56,9 @@ router.post('/', function(req, res, next) {
             throw err;
         }
         console.log("passed init err ");
-        console.log("results: ", JSON.parse(results).pass);
+        console.log("results: ", JSON.stringify(results)[0].pass);
 
-        if (bcrypt.compare(req.body.password, JSON.parse(results).pass)) {
+        if (bcrypt.compare(req.body.password, JSON.stringify(results)[0].pass)) {
             console.log("bcrypt comparison pass");
             res.json({ valid: "ok" }).status(200);
         } else {
