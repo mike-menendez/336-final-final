901031afb74e64631d6afbcbc715f5b348b68be4
Mike Menendez <mmenendez@csumb.edu>
2019-12-16 17:58:43 -0800

    dfjk

--- public/js/all.js
+++ public/js/all.js
@@ -5,10 +5,37 @@ $(document).ready(function() {
         type: "POST",
         data: { u: sessionStorage.getItem("user") },
         success: function(result) {
+            var x = '<div class = "card container shadow p-5">' +
+                '<table><thead><tr><th style = "text-align:left">Block #</th>' +
+                '<th style = "text-align: left>Day</th><th style = "text-align: left>Start</th>' +
+                '<th style = "text-align: left>End</th><th style="text-align:left">Booked</th></tr></thead>';
             result.forEach(element => {
-                console.log(element);
+                x = x + '<tr><td style = "text-align:left>' + element.b_id + '</td>\n' +
+                    '<td style = "text-align:left">' + element.day + '</td>' +
+                    '<td style = "text-align:left">' + element.stime + '</td>' +
+                    '<td style = "text-align:left">' + element.etime + '</td>';
+                if (!element.booked) {
+                    x = x + '<td style = "text-align: left">Not Booked</td>';
+                } else {
+                    x = x + '<td style = "text-align: left" class = "btn btn-error' +
+                        element.b_id + '">Delete</td>';
+                    $("\"." + element.b_id + "\"").on("click", () => {
+                        $.ajax({
+                            url: "https://cst336-final-final-mmenendez.herokuapp.com/user/del",
+                            type: "DELETE",
+                            data: { id: element.b_id },
+                            success: () => {
+                                Swal.fire({
+                                    title: "Deleted Successfully",
+                                    icon: "success"
+                                });
+                            }
+                        });
+                    });
+                }
             });
+            x = x + "</tbody></table></div>";
+            $(".lol").replaceWith(x);
         }
-
     });
 });
